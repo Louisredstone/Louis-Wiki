94819c8475ade61e4a0f68963093aa2fce8949f5
Louisredstone <zzy2900920898@163.com>
2025-02-10 09:46:25 +0800

    Some modification

--- src/wiki_library.ts
+++ src/wiki_library.ts
@@ -21,39 +21,53 @@ interface TFileWithMetadata{
     metadata: CachedMetadata|null;
 }
 
-interface WikiLibrarySummary{
-    updated_at: number;
-    "duplicated-wiki-tags": {
-        [wiki_tag: string]: string[]
-    };
-    "wiki-tags": {
-        [wiki_tag: string]: string[]
-    };
-    files: {
-        [path: string]: FileInfo
-    }
-}
+// interface WikiLibrarySummary{
+//     updated_at: number;
+//     "duplicated-wiki-tags": {
+//         [wiki_tag: string]: string[]
+//     };
+//     "wiki-tags": {
+//         [wiki_tag: string]: string[]
+//     };
+//     files: {
+//         [path: string]: FileInfo
+//     }
+// }
 
+interface SimilarityInfo{
+    fm: TFileWithMetadata;
+    similarity: number;
+    intersection_size: number;
+}
 
 export class WikiLibrary{
     app: App;
     rootFolderPath: string;
     rootFolder: TFolder;
-    summary: WikiLibrarySummary;
+    // summary: WikiLibrarySummary;
     // summaryTFile: TFile;
     folders: TFolder[];
-    entries: TFileWithMetadata[];
+    allNotes: TFileWithMetadata[];
+    wikiEntries: TFileWithMetadata[];
+    disambiguationNotes: TFileWithMetadata[];
     categories: TFileWithMetadata[];
     otherNotes: TFileWithMetadata[];
     entryTemplate: string;
 
-    constructor(app: App, rootFolder: string, entryTemplate: string) {
+    wiki_tag_dict: {[wiki_tag: string]: TFileWithMetadata[]};
+    duplicated_wiki_tags: string[];
+
+    constructor(app: App, rootFolder: string|null, entryTemplate: string) {
+        if (!rootFolder) {
+            error("Root folder not specified, please set it in settings.");
+            return;
+        }
         this.app = app;
         this.rootFolderPath = rootFolder;
         this.entryTemplate = entryTemplate;
         this.init_folders();
         this.init_files();
-        this.init_wiki_summary();
+        this.init_wiki_tags();
     }
 
     async init_folders(): Promise<void> {
@@ -77,18 +91,19 @@ export class WikiLibrary{
     }
 
     async init_files(): Promise<void> {
-        const files: TFileWithMetadata[] = [];
+        const allNotes: TFileWithMetadata[] = [];
         Vault.recurseChildren(this.rootFolder, (tAbstractFile)=>{
             if (tAbstractFile instanceof TFile && tAbstractFile.extension ==='md')
             {
                 const metadata = this.app.metadataCache.getFileCache(tAbstractFile);
-                files.push({file: tAbstractFile, metadata: metadata});
+                allNotes.push({file: tAbstractFile, metadata: metadata});
             }
         });
-        const entries: TFileWithMetadata[] = [];
+        const wikiEntries: TFileWithMetadata[] = [];
         const categories: TFileWithMetadata[] = [];
+        const disambiguationNotes: TFileWithMetadata[] = [];
         const otherFiles: TFileWithMetadata[] = [];
-        for (const file of files) {
+        for (const file of allNotes) {
             const metadata = file.metadata;
             if (!metadata) {
                 otherFiles.push(file);
@@ -100,19 +115,23 @@ export class WikiLibrary{
                 continue;
             }
             if (frontmatter['note-type'] === 'wiki') {
-                entries.push(file);
+                wikiEntries.push(file);
             } else if (frontmatter['note-type'] === 'category') {
                 categories.push(file);
+            } else if (frontmatter['note-type'] === 'disambiguation') {
+                disambiguationNotes.push(file);
             } else {
                 otherFiles.push(file);
             }
         }
-        this.entries = entries;
+        this.allNotes = allNotes;
+        this.wikiEntries = wikiEntries;
         this.categories = categories;
+        this.disambiguationNotes = disambiguationNotes;
         this.otherNotes = otherFiles;
     }
 
-    async init_wiki_summary(): Promise<void> {
+    async init_wiki_tags(): Promise<void> {
         // const summaryPath = this.rootFolderPath + '/wiki-summary.json';
         // const summaryTFile = this.app.vault.getFileByPath(summaryPath);
         // if (!summaryTFile) {
@@ -123,6 +142,33 @@ export class WikiLibrary{
         // this.summary = JSON.parse(await this.app.vault.read(summaryTFile));
         // this.summaryTFile = summaryTFile;
         // TODO: load summary by analyzing this.entries and this.categories
+
+        const wiki_tag_dict: {[wiki_tag: string]: TFileWithMetadata[]} = {};
+        wiki_tag_dict[''] = [];
+        for (const fm of this.wikiEntries) {
+            const metadata = fm.metadata;
+            if (!metadata) {
+                wiki_tag_dict[''].push(fm);
+                continue;
+            }
+            const frontmatter = metadata.frontmatter;
+            if (!frontmatter) {
+                wiki_tag_dict[''].push(fm);
+                continue;
+            }
+            const wiki_tag = frontmatter['wiki-tag'];
+            if (!wiki_tag) {
+                wiki_tag_dict[''].push(fm);
+                continue;
+            }
+            if (wiki_tag in wiki_tag_dict) {
+                wiki_tag_dict[wiki_tag].push(fm);
+            } else {
+                wiki_tag_dict[wiki_tag] = [fm];
+            }
+        }
+        this.wiki_tag_dict = wiki_tag_dict;
+        this.duplicated_wiki_tags = Object.keys(this.wiki_tag_dict).filter(tag => tag!="" && this.wiki_tag_dict[tag].length > 1);
     }
 
     async addNewEntry(folder: string|TFolder, title: string, wiki_tag: string, aliases: string[], tags: string[], description: string, titleItems: string[]): Promise<void> {
@@ -130,7 +176,7 @@ export class WikiLibrary{
         if (similarities.length > 0) {
             const most_similar = similarities[0];
             if (most_similar.similarity > 0.5 && most_similar.intersection_size > 1) {
-                const yes = await yesNoPrompt(this.app, "A very similar note found, cancellation recommended. Or you may consider creating a disambiguation note. Do you want to cancel?", "title: '"+basename(most_similar.path)+"', similarity: "+most_similar.similarity.toFixed(2)+", intersection: "+most_similar.intersection_size);
+                const yes = await yesNoPrompt(this.app, "A very similar note found, cancellation recommended. Or you may consider creating a disambiguation note. Do you want to cancel?", "title: '"+basename(most_similar.fm.file.path)+"', similarity: "+most_similar.similarity.toFixed(2)+", intersection: "+most_similar.intersection_size);
                 // TODO: add disambiguation note
                 if (yes) {
                     console.log("very similar note found, canceled creating a new note");
@@ -160,11 +206,11 @@ export class WikiLibrary{
         }
 
         // # check if note already exists
-        var sameTitleNotePaths = Object.keys(this.summary.files).filter((path) => {basename(path)==title});
-        if (sameTitleNotePaths.length > 0) {
+        var sameTitleNote = this.allNotes.filter(note => note.file.name == title)
+        if (sameTitleNote.length > 0) {
             console.log("note already exists");
             log("Wiki note with the same title already exists, open it instead.");
-            await open_file_by_path(this.app, sameTitleNotePaths[0]);
+            await open_file(this.app, sameTitleNote[0].file);
             return;
         }
 
@@ -183,17 +229,19 @@ export class WikiLibrary{
 
         const newNoteFilePath = folder.path+'/'+title+'.md'
         const newWikiEntry = await this.app.vault.create(newNoteFilePath, content);
+        const cache = this.app.metadataCache.getFileCache(newWikiEntry);
+        // TODO: update frontmatter through cache, rather than stringifyYaml
 
         // # post process wiki entry creation: update wiki summary
         console.log("post process wiki entry creation");
-        if (wiki_tag in this.summary['wiki-tags']) {
-            this.summary['duplicated-wiki-tags'][wiki_tag] = this.summary['wiki-tags'][wiki_tag].concat([newNoteFilePath]);
-            delete this.summary['wiki-tags'][wiki_tag];
+        if (wiki_tag in this.wiki_tag_dict) {
+            this.wiki_tag_dict[wiki_tag].push({file: newWikiEntry, metadata: cache});
+            this.duplicated_wiki_tags.push(wiki_tag);
         } else {
-            this.summary['wiki-tags'][wiki_tag] = [newNoteFilePath];
+            this.wiki_tag_dict[wiki_tag] = [{file: newWikiEntry, metadata: cache}];
         }
-        this.summary['files'][newNoteFilePath] = frontmatter;
-        this.summary.updated_at = new Date().getTime();
+        // this.summary['files'][newNoteFilePath] = frontmatter;
+        // this.summary.updated_at = new Date().getTime();
         // this.app.vault.modify(this.summaryTFile, JSON.stringify(this.summary, null, 2));
 
         // # open note
@@ -202,20 +250,24 @@ export class WikiLibrary{
         log("New Wiki Entry '"+title+"' created successfully!", 3);
     }
 
-    async similarityAnalyze(wiki_tag: string, aliases: string[], titleEntities: string[]): Promise<{path: string, fileInfo: FileInfo, similarity: number, intersection_size: number}[]>{
-        var similarities = Object.entries(this.summary.files).map(([path, fileInfo]) => {
+    async similarityAnalyze(wiki_tag: string, aliases: string[], titleEntities: string[]): Promise<SimilarityInfo[]>{
+        var similarities = this.allNotes.map((fm) => {
+            const metadata = fm.metadata;
+            if (!metadata) return null;
+            const frontmatter = metadata.frontmatter;
+            if (!frontmatter) return null;
             var setA = new Set(titleEntities);
-            var setB = new Set(fileInfo.tags.concat(fileInfo.aliases));
+            var setB = new Set(frontmatter.tags.concat(frontmatter.aliases));
             var intersection = new Set(titleEntities.filter(x => setB.has(x)));
             var similarity = intersection.size / Math.min(setA.size, setB.size);
-            if (wiki_tag == fileInfo['wiki-tag']) {
+            if (wiki_tag == frontmatter['wiki-tag']) {
                 similarity = Math.max(similarity, 1);
-            } else if (fileInfo.tags.concat(fileInfo.aliases).includes(wiki_tag) || aliases.includes(fileInfo['wiki-tag'])){
+            } else if (frontmatter.tags.concat(frontmatter.aliases).includes(wiki_tag) || aliases.includes(frontmatter['wiki-tag'])){
                 similarity = Math.max(similarity, 0.7);
             }
             var intersection_size = intersection.size;
-            return {path, fileInfo, similarity, intersection_size};
-        });
+            return {fm, similarity, intersection_size};
+        }).filter(item => item!= null);
         console.log("DEBUG insert 0");
         similarities = similarities.sort((a, b) => b.similarity - a.similarity).filter(item => item.similarity > 0.5 || item.intersection_size> 1);
         return similarities;
@@ -230,7 +282,7 @@ export class WikiLibrary{
     }
 }
 
-function foundMultipleSimilarNotesPrompt(app: App, similarities: {path: string, fileInfo: FileInfo, similarity: number, intersection_size: number}[]): Promise<boolean|null> {
+function foundMultipleSimilarNotesPrompt(app: App, similarities: SimilarityInfo[]): Promise<boolean|null> {
     var choice: boolean|null = null;
     const modal = new FoundMultipleSimilarNotesModal(app, similarities, async () => {
         choice = true;
@@ -246,13 +298,13 @@ function foundMultipleSimilarNotesPrompt(app: App, similarities: {path: string, 
 }
 
 class FoundMultipleSimilarNotesModal extends Modal {
-    similarities: {path: string, fileInfo: FileInfo, similarity: number, intersection_size: number}[];
+    similaritiesInfo: SimilarityInfo[];
     continueCallback: () => Promise<void>;
     cancelCallback: () => Promise<void>;
 
-    constructor(app: App, similarities: {path: string, fileInfo: FileInfo, similarity: number, intersection_size: number}[], continueCallback: () => Promise<void> = async () => {}, cancelCallback: () => Promise<void> = async () => {}) {
+    constructor(app: App, similaritiesInfo: SimilarityInfo[], continueCallback: () => Promise<void> = async () => {}, cancelCallback: () => Promise<void> = async () => {}) {
         super(app);
-        this.similarities = similarities;
+        this.similaritiesInfo = similaritiesInfo;
         this.continueCallback = continueCallback;
         this.cancelCallback = cancelCallback;
     }
@@ -266,12 +318,19 @@ class FoundMultipleSimilarNotesModal extends Modal {
         headerRow.createEl('th', { text: "Similarity" });
         headerRow.createEl('th', { text: "Intersection" });
         headerRow.createEl('th', { text: "Path" });
-        for (const { path, fileInfo, similarity, intersection_size } of this.similarities) {
+        for (const { fm, similarity, intersection_size } of this.similaritiesInfo) {
             const row = table.createEl('tr');
-            row.createEl('td', { text: fileInfo['wiki-tag'] });
+            var wiki_tag = '-';
+            if (fm.metadata) {
+                const frontmatter = fm.metadata.frontmatter;
+                if (frontmatter) {
+                    wiki_tag = frontmatter['wiki-tag'];
+                }
+            }
+            row.createEl('td', { text: wiki_tag });
             row.createEl('td', { text: similarity.toFixed(2) });
             row.createEl('td', { text: intersection_size.toString() });
-            row.createEl('td', { text: path });
+            row.createEl('td', { text: fm.file.path });
         }
         const buttonRow = contentEl.createDiv();
         const continueButton = new ButtonComponent(buttonRow);
